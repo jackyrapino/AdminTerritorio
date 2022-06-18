a003a6256cbcce96ad7867def69a02bcb0ff1cad
Jacqueline Maquez <jmarquez@valkiria>
2022-06-18 13:15:17 -0300

    creo componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PublicHomeComponent } from './public/pages/public-home/public-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PublicHomeComponent
   ],
   imports: [
     BrowserModule,
