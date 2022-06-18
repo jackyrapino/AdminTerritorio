4468f79692359b731a5bba3e94d8c9dd76febf74
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-06-18 13:22:07 -0300

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PrivateHomeComponent } from './private/pages/private-home/private-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PrivateHomeComponent
   ],
   imports: [
     BrowserModule,
