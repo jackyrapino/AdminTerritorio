a5b825110dffe88e0b984076ab45d731e51bd4cb
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-06-18 13:08:10 -0300

    Pruebas firebase

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private storageSVC: StorageService) {}
 
   ngOnInit(): void {
+    this.getTest();
   }
 
+  getTest() {
+    this.storageSVC.GetAll('test').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
