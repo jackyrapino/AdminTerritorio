d539cd43e5a6491902d697fdb6fb32ea3c64ffb8
Jacqueline Maquez <jmarquez@valkiria>
2022-06-18 23:28:50 -0300

    añadir logica de seleccionar hermano

--- src/app/public/pages/solicitar-territorio/solicitar-territorio.component.ts
+++ src/app/public/pages/solicitar-territorio/solicitar-territorio.component.ts
@@ -8,6 +8,8 @@ import { StorageService } from 'src/app/services/storage.service';
 })
 export class SolicitarTerritorioComponent implements OnInit {
   hermanos: any;
+  nombre: string = '';
+  hermanoSeleccionado: any;
 
   constructor(private storageSVC: StorageService) {}
 
@@ -20,5 +22,22 @@ export class SolicitarTerritorioComponent implements OnInit {
       this.hermanos = hermanos;
       console.log(this.hermanos);
     });
+  }
+
+  seleccionarHermano(hermano: any) {
+    if (this.hermanoSeleccionado) this.removeSelected();
+    this.hermanoSeleccionado = '';
+    this.hermanoSeleccionado = hermano;
+    console.log(this.hermanoSeleccionado);
+
+    let card = document.querySelector(`#${hermano.id}`) as HTMLElement;
+    card.classList.add('selected');
+  }
+
+  removeSelected() {
+    let card = document.querySelector(
+      `#${this.hermanoSeleccionado.id}`
+    ) as HTMLElement;
+    card.classList.remove('selected');
   }
 }
